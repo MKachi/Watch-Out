bdcc393c73b97f8963a91ae719444a6c691c4459
M_Kachi <mkachi@naver.com>
2016-08-20 22:50:25 +0900

    Remove Comment
    Remove

--- Classes/SkeletonObject.h
+++ Classes/SkeletonObject.h
@@ -2,16 +2,7 @@
 
 #include "cocos2d.h"
 #include "cocostudio/CocoStudio.h"
-/*
-SpriteFrameCache* frameCache = SpriteFrameCache::getInstance();
-frameCache->addSpriteFramesWithFile("friendly/char-kr-pack.plist");
-auto rootNode = CSLoader::createNode("friendly/char-kr-sk.csb");
-cocostudio::timeline::ActionTimeline* action = CSLoader::createTimeline("friendly/char-kr-sk.csb");
-action->gotoFrameAndPlay(0, true);
-rootNode->setPosition(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
-rootNode->runAction(action);
-this->addChild(rootNode, 199);
-*/
+ 
 namespace cocos2d
 {
 	class SkeletonObject
